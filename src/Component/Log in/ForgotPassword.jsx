9b8aec068384c03462f82ea935873ed348748d42
mamta744 <mp744678@gmail.com>
2022-05-26 19:29:12 +0530

    forgotpw

--- src/Component/Log in/ForgotPassword.jsx
+++ src/Component/Log in/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { SetNewPassword } from "./SetNewPassword";
 import { Container, Form, Col, Row, Button } from "react-bootstrap";
 
@@ -9,27 +9,58 @@ import { Container, Form, Col, Row, Button } from "react-bootstrap";
 **/
 
 export const ForgotPassword = (props) => {
+  
+  const [LoginForm, setLoginForm] = useState({
+    Email: "",
+  });
+
+  const onClickLogin = () => {
+    console.log(LoginForm);
+  };
+  const OnChangeEmail = (e) => {
+    setLoginForm({ ...LoginForm, Email: e.target.value });
+  };
+
   return (
     <>
-      <Col>
         <Container>
-          <h1>Forgot Password</h1>
-          <p>Enter your email ans we'll send You a link for set password</p>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label align="left">Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+        
+          <row>
+            <Col sm={4}>
+              <Form>
+                <Row>
+                <Form.Text>
+                Enter your email and we'll send You a link for reset password
+                </Form.Text>
+                  <Form.Group className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">
+                      Email
+                    </Form.Label>
+                    <Col sm="10">
+                      <Form.Control
+                        type="Text"
+                        value={LoginForm.Email}
+                        onChange={OnChangeEmail}
+                        placeholder="abcd@gamil.com"
+                      />
+                      <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                    </Col>
+                  </Form.Group>
+                </Row>
+                <Row>
+                <Col>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+                </Col>
+              </Row>
+              </Form>
+            </Col>
+            <Col sm={8}></Col>
+          </row>
         </Container>
-      </Col>
     </>
   );
 };
