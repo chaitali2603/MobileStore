b8e1420e9c9ea7e5e78aab6165d1ccf47d7934fb
mamta744 <mp744678@gmail.com>
2022-06-17 22:41:18 +0530

    Contactus

--- src/Component/Layout/ContactUs/ContactUs.jsx
+++ src/Component/Layout/ContactUs/ContactUs.jsx
@@ -10,8 +10,12 @@ export const ContactUs
  = (props) => {
   return(
       <>
-    <div>ContactUs
-        
+    <div>Shree Sawariyaseth Mobile Shop<br></br>
+      Khodiyar Nagar<br></br>
+      New VIP Road<br></br>
+      Vadodara-390019.<br></br>
+      Mo.- 9857485963<br></br>
+      Shreesawariyasheth@gmail.com        
     </div>
     </>
    )
